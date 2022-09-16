b00aa968ec0e3cf3eda4e378d96e4dea452841a3
Anna <annamosolowa@gmail.com>
2022-09-16 15:33:04 +1000

    Задача 10

---
+++ exercise10/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+/* int n = new Random().Next(100,1000);
+
+int result = n / 10 % 10;
+
+System.Console.WriteLine($"{n}->{result}"); */
+
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = n / 10 % 10;
+
+System.Console.WriteLine($"{n}->{result}");
--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/*  Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.  */
+
+
+/* int n = new Random().Next(100,1000);
+
+int result = n / 10 % 10;
+
+System.Console.WriteLine($"{n}->{result}"); */
+
+
+
+/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+является ли этот день выходным. */
+/* Console.Write ("Введите номер дня недели: ");
+int day = Convert.ToInt32 (Console.ReadLine());
+
+if ( day == 6) {
+    System.Console.WriteLine("Выходной");
+}
+else if ( day == 6) {
+    System.Console.WriteLine("Выходной");
+}
+    
+else if ( day == 1){
+    System.Console.WriteLine("рабочий день");
+}
+else if ( day == 2){
+    System.Console.WriteLine("рабочий день");
+}
+else if ( day == 3){
+    System.Console.WriteLine("рабочий день");
+}
+else if ( day == 4){
+    System.Console.WriteLine("рабочий день");
+}
+else if ( day == 5){
+    System.Console.WriteLine("рабочий день");
+}
+else {
+    System.Console.WriteLine("Неверный день недели");
+} */
+
+
+
