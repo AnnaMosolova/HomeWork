ba3993047940a236eea786e8c092ed66565253cc
Anna <annamosolowa@gmail.com>
2022-09-17 12:16:56 +1000

    Добавили задачу

--- exercize13/Program.cs
+++ exercize13/Program.cs
@@ -1,6 +1,9 @@
 ﻿System.Console.WriteLine("Введите число a: ");
 int a = Convert.ToInt32(Console.ReadLine());
-
-int result = a / 100 % 10;
-System.Console.WriteLine($"{a}->{result}");
-
+ if (a < 100){
+    System.Console.WriteLine("третьей цифры нет");
+ }
+ else{
+    int result = a / 100 % 10;
+    System.Console.WriteLine($"{a}-->{result}");
+ }
